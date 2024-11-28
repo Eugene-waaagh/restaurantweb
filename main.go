42327f725cb81d7d2423c981fe946bb53e896a2d
Eugene <qihao.eugenewong@gmail.com>
2024-11-28 21:23:55 +0800

    Added library for global variables and config for it

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
-package cc_restaurant
+package main
+
+import (
+	"database/sql"
+	"github.com/eugene-waaagh/restaurantweb/db/api"
+	db "github.com/eugene-waaagh/restaurantweb/db/sqlc"
+	"github.com/eugene-waaagh/restaurantweb/util"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDrive, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
